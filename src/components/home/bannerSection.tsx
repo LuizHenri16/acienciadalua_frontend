3d780b2eae94898b1973e732f1f66e310e7b8878
LuizHenri16 <lbastos722@gmail.com>
2026-05-09 19:34:31 -0300

    feature: add bannerSection

---
+++ src/components/home/bannerSection.tsx
@@ -0,0 +1,9 @@
+export function BannerSection() {
+    return (
+        <div className="banner flex flex-col gap-2 px-4 py-6">
+            <p className="text-sm">Estude com <span className="text-green-light-custom">método e intenção</span></p>
+            <h1 className="text-2xl font-bold">Transforme sua <span className="text-green-light-custom">forma de aprender</span></h1>
+            <p className="text-sm">Materiais de estudo para <span className="text-green-light-custom">gabaritar</span> ou dar uma aula que os alunos não esquecem.</p>
+        </div>
+    );
+}
