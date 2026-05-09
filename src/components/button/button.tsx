3223b9ee496f5155a3507056434f16b4026720ed
LuizHenri16 <lbastos722@gmail.com>
2026-05-09 17:40:59 -0300

    feature: add spinner animation on button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,19 +2,22 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
+import { HashLoader, RingLoader } from "react-spinners";
 
 export interface ButtonProps {
     name: string;
+    loadingName: string;
     onClick: () => void;
     variant?: 'primary' | 'secondary';
+    loading?: boolean;
 }
 
-export function Button({ name, onClick, variant = 'primary' }: ButtonProps) {
+export function Button({ name, loadingName, onClick, variant = 'primary', loading = false }: ButtonProps) {
     return (
-        <button className={`w-full py-3.5 mt-5 cursor-pointer rounded-xl shadow-md font-bold transition-colors ${variant === 'primary' ? 'bg-[#68B999] hover:opacity-90 text-white' : 'bg-[#3D6B5C] hover:opacity-90 text-[#E5E5E3]'}`}
-            onClick={onClick}>
-            {name}
+        <button className={`w-full py-4 mt-5 cursor-pointer rounded-xl shadow-md font-bold transition-colors ${variant === 'primary' ? 'bg-[#68B999] hover:opacity-90 text-white' : 'bg-[#3D6B5C] hover:opacity-90 text-[#E5E5E3]'}`}
+            onClick={onClick}
+            disabled={loading}>
+            {loading ? <span className="w-full flex justify-center items-center gap-2 animate-pulse">{loadingName} <HashLoader size={20} color="white" /></span> : <span className="font-medium">{name}</span>}
         </button>
     );
 }
