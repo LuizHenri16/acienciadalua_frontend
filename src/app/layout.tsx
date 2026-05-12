5e0ad42ffb0c49df1ff266a909614da29a33cbf2
LuizHenri16 <lbastos722@gmail.com>
2026-05-12 10:46:43 -0300

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,12 @@ const unkempt = Unkempt({
 });
 
 export const metadata: Metadata = {
-  title: "A Ciência da Lua",
-  description: "A Ciência da Lua",
+  title: "A Ciência da Lua - Materiais de Estudos",
+  description: "E-commerce de materiais de estudos para o Ensino Fundamental e Médio para alunos e professores.",
+  keywords: ["ciências", "ensino fundamental", "ensino médio", "materiais de estudos", "vestibulares", "enem", "estudos"],
+  icons: {
+    icon: '/acienciadalua-logo.svg',
+  },
 };
 
 export default function RootLayout({
