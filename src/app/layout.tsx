af2bbbd198607fc47674c339ce91d71974cf2a8c
LuizHenri16 <lbastos722@gmail.com>
2026-05-12 21:09:01 -0300

    feature: add framer-motion for smooth transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Sora, Unkempt } from "next/font/google";
 import "./globals.css";
+import PageTransition from "@/hooks/framerMotion/PageTransition";
 
 const sora = Sora({
   variable: "--font-sora",
@@ -29,7 +30,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" suppressHydrationWarning className={`${sora.variable} ${unkempt.variable}  h-full antialiased`}>
-      <body className="min-h-full flex flex-col ">{children}</body>
+      <body className="min-h-full flex flex-col ">
+        <PageTransition>
+          {children}
+        </PageTransition>
+      </body>
     </html>
   );
 }
