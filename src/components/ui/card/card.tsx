aab2e92f8af719e8656f66592c66be2dce3ba1be
LuizHenri16 <lbastos722@gmail.com>
2026-05-13 17:21:52 -0300

    style: update card, banner and aboutBanner colors

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Material } from "@/types/material";
+import { ArrowLeftCircleIcon, ArrowRightCircle, ArrowRightFromLine, ArrowRightIcon } from "lucide-react";
 
 interface CardProps {
     material: Material
@@ -11,20 +12,23 @@ function formatPrice(price: number) {
 
 export function ProductCard({ material }: CardProps) {
     return (
-        <div key={material.id} className="bg-white h-80 rounded-2xl shadow-sm overflow-hidden flex flex-col">
+        <div key={material.id} className="bg-white h-84 border border-borda-med squircle-border overflow-hidden flex flex-col">
             <div className="w-full aspect-4/3 bg-[#7c4dbe]" />
             <div className="p-3 flex flex-col gap-2 flex-1">
-                <p className="text-sm font-medium text-[#171717] leading-snug">
+                <p className={`p-0.5 text-[0.6rem] md:text-xs font-normal text-center uppercase squircle-border ${material.type === "Plano de aula" ? "bg-ouro-light text-[#7A5200] w-24 md:w-30" : "bg-turquesa-light text-petroleo w-34 md:w-38"} `}>
+                    {material.type}
+                </p>
+                <p className="text-sm font-medium text-texto-principal leading-snug">
                     {material.name}
                 </p>
-                <p className="text-sm font-bold text-[#171717]">
+                <p className="text-sm font-bold text-texto-principal">
                     {formatPrice(material.price)}
                 </p>
                 <Link
                     href={`/product/${material.id}`}
-                    className="mt-auto w-full cursor-pointer bg-[#3D6B5C] hover:bg-[#2f5549] transition-colors text-white text-xs font-medium py-2 rounded-lg text-center block"
+                    className="flex gap-0.5 mt-auto mb-1 cursor-pointer font-medium transition-colors text-azul-med hover:text-turquesa-dark text-xs"
                 >
-                    ver detalhes
+                    ver detalhes <ArrowRightIcon size={16} />
                 </Link>
             </div>
         </div>
