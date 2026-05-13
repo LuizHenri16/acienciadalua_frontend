7b620f6c129a7eeb0658b3e2e1a599adbe9e42d7
LuizHenri16 <lbastos722@gmail.com>
2026-05-13 17:41:15 -0300

    style: update faq section with new colors

--- src/components/home/faqSection.tsx
+++ src/components/home/faqSection.tsx
@@ -8,9 +8,9 @@ export function FaqSection() {
     return (
         <section className="flex flex-col gap-4">
             <div className="flex flex-col gap-1">
-                <h4 className="text-xs lg:text-sm text-[#5A5A58] uppercase font-bold tracking-wider">Dúvidas frequentes</h4>
-                <h2 className="text-xl lg:text-2xl font-bold ">Perguntas Frequentes</h2>
-                <p className="text-xs lg:text-sm text-[#5A5A58]">As perguntas mais comuns sobre os materiais e como funciona o acesso.</p>
+                <h4 className="text-xs lg:text-sm text-texto-secundario uppercase font-bold tracking-wider">Dúvidas frequentes</h4>
+                <h2 className="text-xl lg:text-2xl text-texto-principal font-bold ">Perguntas Frequentes</h2>
+                <p className="text-xs lg:text-sm tracking-wider text-texto-terciario">As perguntas mais comuns sobre os materiais e como funciona o acesso.</p>
             </div>
 
             {faqContent.map((faq, index) => (
